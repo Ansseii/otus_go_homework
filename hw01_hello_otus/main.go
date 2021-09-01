87fe1427d63c40b881aa7fb9bc2d77751d4df196
Aleksandr Lukash <a.lukash.92@gmail.com>
2021-09-02 01:17:56 +0300

    Add HW1 solution

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	sample := "Hello, OTUS!"
+	fmt.Print(stringutil.Reverse(sample))
 }
