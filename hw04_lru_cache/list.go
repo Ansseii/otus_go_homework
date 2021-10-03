3f4fa7de0303ddf1ed7c7ef1a5105b4bc3c70813
Aleksandr Lukash <a.lukash.92@gmail.com>
2021-10-04 02:02:27 +0300

    Add HW4 solution

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,96 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	size  int
+	first *ListItem
+	last  *ListItem
+}
+
+func (l *list) Len() int {
+	return l.size
+}
+
+func (l *list) Front() *ListItem {
+	return l.first
+}
+
+func (l *list) Back() *ListItem {
+	return l.last
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	var node *ListItem
+
+	if l.first == nil {
+		node = &ListItem{Value: v}
+		l.last = node
+	} else {
+		node = &ListItem{Value: v, Next: l.first}
+		l.first.Prev = node
+	}
+	l.first = node
+	l.size++
+
+	return l.first
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	var node *ListItem
+
+	if l.last == nil {
+		node = &ListItem{Value: v}
+		l.first = node
+	} else {
+		node = &ListItem{Value: v, Prev: l.last}
+		l.last.Next = node
+	}
+	l.last = node
+	l.size++
+
+	return l.last
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i.Prev == nil {
+		l.first = i.Next
+	} else {
+		i.Prev.Next = i.Next
+	}
+
+	if i.Next == nil {
+		l.last = i.Prev
+	} else {
+		i.Next.Prev = i.Prev
+	}
+
+	if l.size != 0 {
+		l.size--
+	}
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	l.PushFront(i.Value)
+	l.Remove(i)
+}
+
+func GetAll(l List) []interface{} {
+	elems := make([]interface{}, 0, l.Len())
+
+	for i := l.Front(); i != nil; i = i.Next {
+		elems = append(elems, i.Value)
+	}
+
+	return elems
+}
+
+func GetAllReversed(l List) []interface{} {
+	elems := make([]interface{}, 0, l.Len())
+
+	for i := l.Back(); i != nil; i = i.Prev {
+		elems = append(elems, i.Value)
+	}
+
+	return elems
 }
 
 func NewList() List {
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -42,10 +42,86 @@ func TestList(t *testing.T) {
 		l.MoveToFront(l.Front()) // [80, 60, 40, 10, 30, 50, 70]
 		l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
 
-		elems := make([]int, 0, l.Len())
-		for i := l.Front(); i != nil; i = i.Next {
-			elems = append(elems, i.Value.(int))
+		elems := make([]int, l.Len())
+		for i, v := range GetAll(l) {
+			elems[i] = v.(int)
 		}
+
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
+
+		reversed := make([]int, l.Len())
+		for i, v := range GetAllReversed(l) {
+			reversed[i] = v.(int)
+		}
+
+		require.Equal(t, []int{50, 30, 10, 40, 60, 80, 70}, reversed)
+	})
+
+	t.Run("remove from empty list", func(t *testing.T) {
+		l := NewList()
+
+		node := l.PushFront(10)
+		l.Remove(node)
+		l.Remove(node)
+
+		require.Equal(t, 0, l.Len())
+	})
+
+	t.Run("remove outsides", func(t *testing.T) {
+		l := NewList()
+
+		first := l.PushBack(1)
+		second := l.PushBack(2)
+		third := l.PushBack(3)
+		fourth := l.PushBack(4)
+
+		l.Remove(fourth)
+		l.Remove(third)
+
+		require.Equal(t, l.Back(), second)
+		require.Equal(t, l.Front(), first)
+
+		l = NewList()
+
+		first = l.PushBack(1)
+		second = l.PushBack(2)
+		third = l.PushBack(3)
+		fourth = l.PushBack(4)
+
+		l.Remove(first)
+		l.Remove(second)
+
+		require.Equal(t, l.Front(), third)
+		require.Equal(t, l.Back(), fourth)
+	})
+
+	t.Run("push in a specific side", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(1)
+		l.PushFront(2)
+		l.PushFront(3)
+		l.PushFront(4)
+
+		elems := make([]int, l.Len())
+		for i, v := range GetAll(l) {
+			elems[i] = v.(int)
+		}
+
+		require.Equal(t, []int{4, 3, 2, 1}, elems)
+
+		l = NewList()
+
+		l.PushBack(1)
+		l.PushBack(2)
+		l.PushBack(3)
+		l.PushBack(4)
+
+		elems = make([]int, l.Len())
+		for i, v := range GetAll(l) {
+			elems[i] = v.(int)
+		}
+
+		require.Equal(t, []int{1, 2, 3, 4}, elems)
 	})
 }
