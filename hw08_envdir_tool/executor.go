0a5cfb0c8771760d52f86a577a1dd1e659631b4f
Aleksandr Lukash <a.lukash.92@gmail.com>
2021-11-28 23:48:21 +0300

    Add HW8 solution

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,33 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	for k, v := range env {
+		if v.NeedRemove {
+			if err := os.Unsetenv(k); err != nil {
+				log.Fatal(err)
+			}
+		}
+		if err := os.Setenv(k, v.Value); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	command, params := cmd[0], cmd[1:]
+
+	e := exec.Command(command, params...)
+	e.Stdin = os.Stdin
+	e.Stdout = os.Stdout
+
+	if err := e.Run(); err != nil {
+		log.Println(err)
+	}
+
+	return e.ProcessState.ExitCode()
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,19 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("Expected return code", func(t *testing.T) {
+		status := RunCmd([]string{"bash", "-c", "exit 10"}, Environment{})
+		require.Equal(t, 10, status)
+	})
+
+	t.Run("Zero code in normal way", func(t *testing.T) {
+		status := RunCmd([]string{"echo", "arg1=1", "arg2=2"}, Environment{})
+		require.Equal(t, 0, status)
+	})
 }
